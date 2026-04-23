906ef30f8a8eff448f0fa464ede14e1564628d65
Javier Dileo <javierdileo84@hotmail.com>
2026-04-23 20:26:57 -0300

    Fix: Correccion para deploy de parseo de imagenes

--- app/services/parsers/image_parse.py
+++ app/services/parsers/image_parse.py
@@ -2,11 +2,18 @@ import io
 import os
 import pytesseract
 from PIL import Image
+import shutil
 
 # Configuración de ruta para Windows (local)
-# En el servidor de la empresa, esto se manejará vía variables de entorno
-tesseract_path = os.getenv("TESSERACT_CMD", r'D:\tesseract\tesseract.exe')
-pytesseract.pytesseract.tesseract_cmd = tesseract_path
+# 1. Buscamos dónde está instalado tesseract en el sistema
+# shutil.which busca el ejecutable automáticamente en el PATH de Linux
+tesseract_bin = shutil.which("tesseract")
+
+if tesseract_bin:
+    pytesseract.pytesseract.tesseract_cmd = tesseract_bin
+else:
+    pytesseract.pytesseract.tesseract_cmd = r'D:\tesseract\tesseract.exe'
+
 
 def parse_image(file_bytes: bytes) -> str:
     imagen = Image.open(io.BytesIO(file_bytes))
